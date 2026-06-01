af98fae9f4937a8a59dbcfac32a9cebb244b808b
mangell16 <60274854+mangell16@users.noreply.github.com>
2026-06-01 17:06:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,17 @@ import os
 # from snowflake.snowpark.context import get_active_session - removed here in github
 from snowflake.snowpark.functions import col
 
+# session  = get_active_session()   -- removed here in github, replaced with next 2 lines
+cnx = st.connection("snowflake")
+session = cnx.session() 
+
+
 # Write directly to the app
 st.title(f":cup_with_straw: Please place your custom Smoothie Order ")
 st.write(
   """Please select up to 5 fruits !
   """
 )
-# session  = get_active_session()   -- removed here in github, replaced with next 2 lines
-cnx = st.connection("snowflake")
-session = cnx.session() 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
@@ -49,41 +51,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="?")
         
-# st.markdown("""
-# - :page_with_curl: [Streamlit open source documentation] (https://docs.streamlit.io)
-# - :snowflake: [Streamlit in Snowflake documentation](https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit)
-# - :books: [Demo repo with templates](https://github.com/Snowflake-Labs/snowflake-demo-streamlit)
-# - :memo: [Streamlit in Snowflake release notes](https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake)
-#   """)
 
-# Create a database connection to Snowflake
-# conn = st.connection("snowflake", ttl=os.getenv("SNOWFLAKE_CONNECTION_TTL"))
-# session = conn.session()
-
-# Use an interactive slider to get user input
-# hifives_val = st.slider(
-#   "Number of high-fives in Q3",
-#   min_value=0,
-#   max_value=90,
-#   value=60,
-#   help="Use this to enter the number of high-fives you gave in Q3",
-# )
-
-#  Create an example dataframe
-#  Note: this is just some dummy data, but you can easily connect to your Snowflake data
-#  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
-# created_dataframe = session.create_dataframe(
-#   [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
-#   schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
-# )
-
-# Execute the query and convert it into a Pandas dataframe
-# queried_data = created_dataframe.to_pandas()
-
-# Create a simple bar chart
-# See docs.streamlit.io for more types of charts
-# st.subheader("Number of high-fives")
-# st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
-
-# st.subheader("Underlying data")
-# st.dataframe(queried_data, use_container_width=True)
