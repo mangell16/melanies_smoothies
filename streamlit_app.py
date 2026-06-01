cc03250df1bab8f1df0f03d049ae48be32431a96
mangell16 <60274854+mangell16@users.noreply.github.com>
2026-06-01 19:48:59 -0400

    Update streamlit_app.py
    once again adjust smoothifroot call

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if ingredients_list:
           # st.text(smoothiefroot_response.json())
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon]")  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
    # smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
